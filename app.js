dd2451eebdcc0083dbae6b2d169b5374f7c8a816
Himanshu <himanshu.hn.1998@gmail.com>
2023-07-29 22:38:03 +0530

    imported dependencies into app.js and setup concurrently

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
-const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
+const app = express();
 const PORT = process.env.PORT || 8000;
 
+require('dotenv').config();
+
+const corsOptions = {
+    credentials: true,
+    origin: true
+};
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cors(corsOptions));
+
 // listining for port
-const server = app.listen(PORT, function () {
-    console.log(`TryAR running on port ${PORT}....`);
-});
+app.listen(PORT, () => console.log(`TryAR running on port ${PORT}....`));
