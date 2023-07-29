2d269ec73a28fc8bc3d81fc42d21ec46d5d372ce
Himanshu <himanshu.hn.1998@gmail.com>
2023-07-29 23:59:19 +0530

    added the models and routes from previous project. Still have to change these code file for this project

---
+++ model/users.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var user = new Schema({
+    userName: String,
+    googleId: String,
+    userEmail: String,
+    userImage: String,
+    userPhoneNo: [{
+        value: String,
+        verified: {
+            type: Boolean,
+            default: false
+        }
+    }]
+});
+
+mongoose.model('User', user);
---
+++ rotues/auth-routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+var passport = require('passport');
+const logger = require('../config/logger');
+
+router.get(
+    "/auth/google",
+    passport.authenticate("google", {
+        scope: ["profile", "email"]
+    })
+);
+
+router.get(
+    "/auth/google/callback",
+    passport.authenticate("google"),
+    (req, res) => {
+        logger.log('info', 'logged in successfully');
+        res.redirect("/shops");
+    }
+);
+
+router.get("/current_user", (req, res) => {
+    res.send(req.user);
+});
+
+router.get("/auth/logout", (req, res) => {
+    req.logout();
+    logger.log('info', 'user logged out successfully');
+    res.redirect('/');
+});
+
+module.exports = router;
