ad1f9caf208d50b4c85bcf6112090a3b083cc155
Himanshu <himanshu.hn.1998@gmail.com>
2023-07-29 23:10:00 +0530

    setup the config folder with keys.js and prod_keys.js

---
+++ config/prod_keys.js
@@ -0,0 +1,10 @@
+module.exports = {
+    google: {
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET
+    },
+    /*session: {
+        cookieKey: process.env.COOKIE_KEY
+    },*/
+    mongoURI: process.env.MONGO_URI
+};
