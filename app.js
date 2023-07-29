6b106c190c48ea1f49037dfaf47395ed75ef2b9d
Himanshu <himanshu.hn.1998@gmail.com>
2023-07-29 07:29:10 +0530

    client created using vite react.js

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 // listining for port
-var server = app.listen(PORT, function () {
+const server = app.listen(PORT, function () {
     console.log(`TryAR running on port ${PORT}....`);
 });
