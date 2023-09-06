c2946d3dcc31b4307c27fae9dd171c11f40051e9
Himanshu <himanshu.hn.1998@gmail.com>
2023-09-06 07:51:45 +0530

    added itemModel prop, to get model location for rendering it. added propTypes

--- client/src/components/ar/Model.jsx
+++ client/src/components/ar/Model.jsx
@@ -1,11 +1,22 @@
-import { useLoader } from '@react-three/fiber'
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { TextureLoader } from 'three/src/loaders/TextureLoader'
+import { useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { TextureLoader } from 'three/src/loaders/TextureLoader';
+import PropTypes from "prop-types";
 
-const Model = ({ position }) => { //eslint-disable-line
-    const gltf = useLoader(GLTFLoader, '/src/assets/1.glb') //pathing is different, it is relative to root file instead of this file
+const Model = ({ position, itemModel }) => {
+    const gltf = useLoader(GLTFLoader, '/src/assets/1.glb') //useLoader(GLTFLoader, itemModel) //pathing is different, it is relative to root file instead of this file
     const texture = useLoader(TextureLoader, '/src/assets/photo_studio_01_1k.jpg')
     return <primitive position={position} object={gltf.scene} material={{map: texture}}/> //eslint-disable-line
 };
+
+Model.propTypes = {
+    position: PropTypes.shape({
+        // specify the types of the properties of the position object
+        x: PropTypes.number,
+        y: PropTypes.number,
+        z: PropTypes.number,
+      }).isRequired,
+      itemModel: PropTypes.string.isRequired,
+  };
 
 export default Model;
