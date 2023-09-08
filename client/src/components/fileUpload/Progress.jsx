4f93b89108c4c764c964bfb5a5c68e35a819c6dc
Himanshu <himanshu.hn.1998@gmail.com>
2023-09-08 06:25:01 +0530

    some minor changes

--- client/src/components/fileUpload/Progress.jsx
+++ client/src/components/fileUpload/Progress.jsx
@@ -5,7 +5,7 @@ const Progress = ({ percentage }) => {
     <div className='relative'>
       <progress className="w-full" value={percentage} max="100" />
       <div className="absolute inset-0 flex items-center justify-center">
-        <span>{percentage}%</span>
+        <span className="text-gray-700 font-bold">{percentage}%</span>
       </div>
     </div>
   );
