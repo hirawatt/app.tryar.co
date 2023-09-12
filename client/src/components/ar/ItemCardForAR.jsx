1bdd53930439d50d500818a179d37264adad6cd8
Himanshu <himanshu.hn.1998@gmail.com>
2023-09-12 23:03:19 +0530

    some minor changes

--- client/src/components/ar/ItemCardForAR.jsx
+++ client/src/components/ar/ItemCardForAR.jsx
@@ -1,17 +1,15 @@
 import TryARLogo from '../../assets/tryAR-logo.png';
 import PropTypes from 'prop-types';
 
-const ItemCardForAR = ({ itemImg, itemName }) => {
-  return (
+const ItemCardForAR = ({ imgLocation, itemName }) => (
     <div className='border border-gray-400 p-4'>
-      <img src={TryARLogo /*itemImg*/} className='h-56 mx-auto' />
+      <img src={TryARLogo || imgLocation} className='h-56 mx-auto' />
       <p className='text-center mx-auto'>{itemName}</p>
     </div>
   )
-}
 
 ItemCardForAR.propTypes = {
-  itemImg: PropTypes.string.isRequired,
+  imgLocation: PropTypes.string.isRequired,
   itemName: PropTypes.string.isRequired
 };
 
